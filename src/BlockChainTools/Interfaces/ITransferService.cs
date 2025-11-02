5c46eb4d102f39c54dfa048ae7e62c88d3e0a238
Hamed Ansarian <hamed.ansarian@hotmail.com>
2025-11-02 17:31:53 +0100

    Add TransferAsync method for ERC721 token transfer

--- src/BlockChainTools/Interfaces/ITransferService.cs
+++ src/BlockChainTools/Interfaces/ITransferService.cs
@@ -30,6 +30,16 @@ public interface ITransferService
     /// <param name="contractAddress"></param>
     /// <param name="request"></param>
     /// <returns></returns>
+    Task<string> TransferAsync(Web3 web3, string contractAddress,
+        Nethereum.Contracts.Standards.ERC721.ContractDefinition.TransferFromFunction request);
+
+    /// <summary>
+    /// Transfer ERC721 token
+    /// </summary>
+    /// <param name="web3"></param>
+    /// <param name="contractAddress"></param>
+    /// <param name="request"></param>
+    /// <returns></returns>
     Task<Nethereum.RPC.Eth.DTOs.TransactionReceipt> TransferAndWaitForReceiptAsync(Web3 web3, Nethereum.RPC.Eth.DTOs.TransactionInput request);
 
     /// <summary>
@@ -41,4 +51,14 @@ public interface ITransferService
     /// <returns></returns>
     Task<Nethereum.RPC.Eth.DTOs.TransactionReceipt> TransferAndWaitForReceiptAsync(Web3 web3, string contractAddress,
         Nethereum.Contracts.Standards.ERC20.ContractDefinition.TransferFunction request);
+
+    /// <summary>
+    /// Transfer ERC20 token and wait for receipt
+    /// </summary>
+    /// <param name="web3"></param>
+    /// <param name="contractAddress"></param>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    Task<Nethereum.RPC.Eth.DTOs.TransactionReceipt> TransferAndWaitForReceiptAsync(Web3 web3, string contractAddress,
+        Nethereum.Contracts.Standards.ERC721.ContractDefinition.TransferFromFunction request);
 }
